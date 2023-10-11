f8c9e1bb4da0d9e1fc774b3695967cad46d4b83c
Admin <1729374397@qq.com>
2023-10-11 18:10:06 +0800

    。

---
+++ home/banner/index.php
@@ -0,0 +1,35 @@
+<?php 
+
+    // require_once 'E:\server\phpstudy\phpstudy_pro\WWW\server\db\connect_db.php';
+    require_once 'D:\Server\PhpStudy\phpstudy_pro\WWW\server\db\connect_db.php';
+
+    if($_SERVER['REQUEST_METHOD'] <> 'GET') {
+          echo '请求方式错误';
+        return;
+    } 
+
+    $query = "SELECT * FROM banner";
+    $stm = $pdo->prepare($query);
+    $stm->execute();
+    $result =  $stm->fetchAll();
+    $data = array();
+    if(count($result) > 0) {
+        foreach ($result as $row) {
+             $data[] = $row;
+        }
+       
+        $output = array(
+            'code' => "0",
+            'msg' => "请求成功",
+            'data' => $data,
+        );
+    } else {
+         $output = array(
+            'code' => "0",
+            'msg' => "暂无数据",
+            'data' => $data,
+        );
+    }
+   
+    echo json_encode($output);
+ ?>
