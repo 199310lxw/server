ff29ba354f36bbf425a39be2dc5f55b5fb8b0632
Admin <1729374397@qq.com>
2023-10-12 18:13:48 +0800

    。

--- home/hot/index.php
+++ home/hot/index.php
@@ -2,41 +2,48 @@
 
     // require_once 'E:\server\phpstudy\phpstudy_pro\WWW\server\db\connect_db.php';
     require_once 'D:\Server\PhpStudy\phpstudy_pro\WWW\server\db\connect_db.php';
-
+    require_once 'D:\Server\PhpStudy\phpstudy_pro\WWW\server\session\session.php';
+   
     $page    = $_GET['page'];
     $size    = $_GET['size'];
  
     $low   = (($page - 1) * $size + 1);
     $hight = $page * $size;
+    $sessionId = $_SERVER['HTTP_SESSION'];
 
     if($_SERVER['REQUEST_METHOD'] <> 'GET') {
           echo '请求方式错误';
         return;
     } 
 
-    $query = "SELECT * FROM hot WHERE id BETWEEN $low AND $hight ";
-    $stm = $pdo->prepare($query);
-    $stm->execute();
-    $result =  $stm->fetchAll();
-
     $data = array();
-    if(count($result) > 0) {
-        foreach ($result as $row) {
-             $data[] = $row;
-        }
-       
-        $output = array(
+   
+    getResult($pdo,$low,$hight);
+
+    function getResult($pdo,$low,$hight) {
+         $query = "SELECT * FROM hot WHERE id BETWEEN $low AND $hight ";
+         $stmt = $pdo->prepare($query);
+         $stmt->execute();
+         $result =  $stmt->fetchAll();
+
+    
+        if(count($result) > 0) {
+            foreach ($result as $row) {
+                    $data[] = $row;
+                }
+           $output = array(
             'code' => "0",
             'msg' => "请求成功",
             'data' => $data,
-        );
-    } else {
-         $output = array(
-            'code' => "0",
-            'msg' => "暂无数据",
-            'data' => $data,
-        );
+           );
+        } else {
+              $output = array(
+                  'code' => "0",
+                  'msg' => "暂无数据",
+                 'data' => $data,
+              );
+        }
+        echo json_encode($output);
     }
-   
-    echo json_encode($output);
+ 
  ?>
---
+++ user/login/index.php
@@ -0,0 +1,71 @@
+<?php 
+
+   require_once 'D:\Server\PhpStudy\phpstudy_pro\WWW\server\db\connect_db.php';
+   require_once 'D:\Server\PhpStudy\phpstudy_pro\WWW\server\session\session.php';
+
+   session_start();
+
+
+   if($_SERVER['REQUEST_METHOD'] <> 'POST') {
+          $output = array(
+            'code' => "405",
+            'msg' => "",
+            'data' => "wrong request method",
+        );
+        echo json_encode($output);
+        return;
+    } 
+
+    $phone = $_POST['phone'];
+    $password = $_POST['password'];
+  
+    $query_search = "SELECT phone FROM user WHERE phone = '$phone' ";
+    $stmt = $pdo->prepare($query_search);
+    $stmt->execute();
+    $result =  $stmt->fetchAll();
+
+  
+    if(count($result) <= 0) {
+       $output = array(
+            'code' => "-1002",
+            'msg' => "用户不存在",
+            'data' => null,
+        );
+    } else {
+         $query =  "SELECT * FROM user WHERE phone = '$phone' AND password = '$password' ";
+         $stmt = $pdo->prepare($query);
+         $stmt->execute();
+         $result1 =  $stmt->fetchAll();
+          if(count($result1) <= 0) { 
+              $output = array(
+                  'code' => "-1003",
+                  'msg' => "密码错误，请重试",
+                  'data' => null,
+              );
+          } else {
+              $session_data = array (
+                  'phone' => $phone,
+                  'password' => $password,
+              );
+            //  $_SESSION['phone'] = $phone;
+            //  $_SESSION['pwssword'] = $password;
+             $session = new Session();
+             $session->setSession('user',$session_data,60);
+        
+              foreach ($result1 as $row) {
+                   $data["phone"] = $row["phone"];
+                   $data["headUrl"] = $row["headUrl"];
+                   $data["name"] = $row["name"];
+                   $data["session"] = session_id();
+              }
+             $output = array(
+                  'code' => "0",
+                  'msg' => "登陆成功",
+                  'data' => $data,
+              );
+
+          }
+    }
+
+   echo json_encode($output);
+ ?>
